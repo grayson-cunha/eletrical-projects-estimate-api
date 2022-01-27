7275e7fc65edbb3f10de73455c9867c4a61fd818
Grayson Cunha <graysonsilva93@gmail.com>
2022-01-27 11:59:01 -0300

    fix: apply eslint and prettier rules on server.ts

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,4 +1,4 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 const dotenvResult = dotenv.config();
 
@@ -6,8 +6,8 @@ if (dotenvResult.error) {
   throw dotenvResult.error;
 }
 
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
 const app: express.Application = express();
 
@@ -15,5 +15,5 @@ app.use(express.json());
 app.use(cors());
 
 app.listen(process.env.PORT, () => {
-  console.log("App listening on port: ", process.env.PORT);
+  console.log('App listening on port: ', process.env.PORT);
 });
