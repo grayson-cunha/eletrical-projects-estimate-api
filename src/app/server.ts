4f4030918a106000aa0caa11b76449c1e08cdab1
Grayson Cunha <graysonsilva93@gmail.com>
2022-01-27 11:53:40 -0300

    initial commit

---
+++ src/app/server.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+
+const dotenvResult = dotenv.config();
+
+if (dotenvResult.error) {
+  throw dotenvResult.error;
+}
+
+import express from "express";
+import cors from "cors";
+
+const app: express.Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT, () => {
+  console.log("App listening on port: ", process.env.PORT);
+});
