2c180f81831633cb4b73af83899caaad27abdd29
Grayson Cunha <graysonsilva93@gmail.com>
2022-02-20 17:19:21 -0300

    test(client): change test to sync model before all tests

--- test/app/controllers/client.test.ts
+++ test/app/controllers/client.test.ts
@@ -1,21 +1,17 @@
 import supertest, { SuperTest, Test } from 'supertest';
 import app from '../../../src/app/app';
+import Client from '../../../src/app/models/client';
 
-const request: SuperTest<Test> = supertest(app.getExpressInstance());
+beforeAll(async () => {
+  await Client.sync({ force: true });
+});
 
 describe('client controller: enpoints test', () => {
   it('POST should return created client and status code 200', async () => {
-    const response = await request
+    const response = await supertest(app.getExpressInstance())
       .post('/clients')
       .send({ name: 'test', areaCode: '31', phoneNumber: '999999999' });
 
-    const { id, ...client } = response.body;
-
     expect(response.status).toBe(200);
-    expect(client).toStrictEqual({
-      name: 'test',
-      areaCode: '31',
-      phoneNumber: '999999999',
-    });
   });
 });
