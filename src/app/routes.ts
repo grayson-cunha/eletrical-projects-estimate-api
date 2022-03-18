945727fad784a62eba1ddf0128cb096de94a14af
Grayson Cunha <graysonsilva93@gmail.com>
2022-03-17 21:49:48 -0300

    fea(estimate): include routes to estimate CRUD endpoints

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import addressRouter from './controllers/address/address.routes';
 import clientRouter from './controllers/client/client.routes';
+import estimateRouter from './controllers/estimate/estimate.routes';
 
 const routes = Router();
 
 routes.use('/clients', clientRouter);
 routes.use('/addresses', addressRouter);
+routes.use('/estimates', estimateRouter);
 
 export default routes;
